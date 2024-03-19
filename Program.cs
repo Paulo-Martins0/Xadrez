e2a25cac61a0d6264144fcd4c9903d5b4de76c5a
Paulo Henrique <paulohenriquemr555@gmail.com>
2024-03-19 13:46:42 -0300

    Xadrez final

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                Console.Write("|*|");
+                for (int j = 1; j <= 2; j++)
+                {
+                    Console.Write("|*|");
+
+
+                }
+                Console.WriteLine();
+                
+            }
+
+            Console.WriteLine("Você escolhe X ou O ? ");
+            Char escolha = Char.Parse(Console.ReadLine());
+
+        }
+    }
+}
